e3fa2f327e4cf825d3779e3dc0a15713eddb2f2c
Reward Steve <rewardstephen30@gmail.com>
2025-06-21 18:40:57 +0100

    update: framer-motion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-import "./globals.css";
-
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { PopupWidget } from "@/components/PopupWidget";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
